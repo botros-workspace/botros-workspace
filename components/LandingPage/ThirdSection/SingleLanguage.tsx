804c2e35b5a30e430e02ddc6bef8738d1cdf9c6c
botros-workspace <botros-workspace@hotmail.com>
2024-03-25 07:56:02 +0100

    language container hover bug fixed

--- components/LandingPage/ThirdSection/SingleLanguage.tsx
+++ components/LandingPage/ThirdSection/SingleLanguage.tsx
@@ -23,32 +23,27 @@ const SingleLanguage: FunctionComponent<Props> = ({
       <Flex
         flexDir={'column'}
         cursor={'pointer'}
-        w={{ base: 24, md: 28, xl: 40 }}
-        gap={6}
+        w={{ base: 24, md: 40, lg: 32, xl: 40 }}
+        gap={1}
         borderRadius={'lg'}
         borderWidth={1}
-        p={4}
+        p={1}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         <Text
-          transform={{
-            base: 'scaleY(2) scaleX(2)',
-            md: 'scaleY(2.7) scaleX(2.7)',
-          }}
           color={'rgb(197,	154	,39,1)'}
           textAlign={'center'}
-          fontSize={{ base: 12, lg: 14, xl: 16 }}
+          fontSize={{ base: 28, md: 40 }}
           fontWeight={700}
-          w={'100%'}
         >
           {shortcut}
         </Text>
         <Text
           color={'white'}
-          fontSize={{ base: 14, md: 16 }}
+          fontSize={{ base: 14, md: 20 }}
           fontWeight={700}
-          w={'100%'}
+          pb={2}
           textAlign={'center'}
           animation={isHovered ? 'bounce ease-in-out 0.6s both' : ''}
         >
